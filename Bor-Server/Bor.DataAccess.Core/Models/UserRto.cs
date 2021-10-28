2259001d774fa40ee12cecb9ab46f29624a58dff
tixdim <82827317+NEXT-GM@users.noreply.github.com>
2021-10-28 19:24:06 +0300

    Начал работать с UsertRto

---
+++ Bor-Server/Bor.DataAccess.Core/Models/UserRto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bor.DataAccess.Core.Models
+{
+    [Table("User")]
+    class UserRto
+    {
+        [Key] public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string PhoneNumberPrefix { get; set; }
+        
+        public string PhoneNumber { get; set; }
+    }
+}
