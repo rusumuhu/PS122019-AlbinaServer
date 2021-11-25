071f1eba72921a08aa69598b94c7d3769b33823d
TitikUniverse <semyon.titov.203@mail.ru>
2021-11-25 19:19:54 +0300

    Закончил БД, начал бизнес логику

---
+++ Bor-Server/Bor.DataAccess/DbContexts/AlbinaContext.cs
@@ -0,0 +1,23 @@
+﻿using Bor.DataAccess.Core.Interfaces;
+using Bor.DataAccess.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bor.DataAccess.DbContexts
+{
+    public class AlbinaContext : DbContext, IContext
+    {
+        public AlbinaContext(DbContextOptions<AlbinaContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserRto> Users { get; set; }
+        public DbSet<VirtualExpenseRto> VirtualExpenses { get; set; }
+        public DbSet<RejectExpenseRto> RejectExpenses { get; set; }
+        public DbSet<AcceptExpenseRto> AcceptExpenses { get; set; }
+    }
+}
