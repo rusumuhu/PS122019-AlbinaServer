00594114205b6bea1b426133a1e98513c6d7ae95
TitikUniverse <semyon.titov.203@mail.ru>
2021-11-30 19:27:37 +0300

    Сделали маппинг и интерфейсы сервисов

--- Bor-Server/Albina.BusinesLogic.Core/Models/UserUpdateBlo.cs
+++ Bor-Server/Albina.BusinesLogic.Core/Models/UserUpdateBlo.cs
@@ -9,7 +9,7 @@ namespace Albina.BusinesLogic.Core.Models
     public class UserUpdateBlo
     {
         public string Name { get; set; }
-        public string FirstName { get; set; }
+        public string Surname { get; set; }
         public string Password { get; set; }
         public string ImageUrl { get; set; }
     }
--- Bor-Server/Albina.BusinesLogic/AutoMapperProfiles/BusinessLogicProfile.cs
+++ Bor-Server/Albina.BusinesLogic/AutoMapperProfiles/BusinessLogicProfile.cs
@@ -14,6 +14,24 @@ namespace Albina.BusinesLogic.AutoMapperProfiles
         public BusinessLogicProfile()
         {
             CreateMap<UserRto, UserInformationBlo>()
+                .ForMember(x => x.Name, x => x.MapFrom(m => m.Name))
+                .ForMember(x => x.Surname, x => x.MapFrom(m => m.Surname))
+                .ForMember(x => x.PhoneNumberPrefix, x => x.MapFrom(m => m.PhoneNumberPrefix))
+                .ForMember(x => x.PhoneNumber, x => x.MapFrom(m => m.PhoneNumber))
+                .ForMember(x => x.ImageUrl, x => x.MapFrom(m => m.ImageUrl));
+
+            CreateMap<UserRto, UserIdentityBlo>()
+                .ForMember(x => x.Id, x => x.MapFrom(m => m.Id))
+                .ForMember(x => x.NumberPrefix, x => x.MapFrom(m => m.PhoneNumberPrefix))
+                .ForMember(x => x.Number, x => x.MapFrom(m => m.PhoneNumber))
+                .ForMember(x => x.Password, x => x.MapFrom(m => m.Password))
+                .ForMember(x => x.ConfirmPassword, m => m.Ignore());
+
+            CreateMap<UserRto, UserUpdateBlo>()
+                .ForMember(x => x.Name, x => x.MapFrom(m => m.Name))
+                .ForMember(x => x.Surname, x => x.MapFrom(m => m.Surname))
+                .ForMember(x => x.Password, x => x.MapFrom(m => m.Password))
+                .ForMember(x => x.ImageUrl, x => x.MapFrom(m => m.ImageUrl));
         }
     }
 }
